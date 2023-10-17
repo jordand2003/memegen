66216cef23ee8678cee22fe162587678726090f3
jordand2003 <jordandominguez600@gmail.com>
2023-10-17 17:40:23 -0400

    Added functionality to "get new image" button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,36 @@
 import React from "react"
+import memesData from "./memesData.js"
 
 export default function Body(){
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getNewImage(){
+        const memeArray = memesData.data.memes;
+        const randomIndex = Math.floor(Math.random() * memeArray.length);
+        setMemeImage(memeArray[randomIndex].url);
+    }
+
     return (
         <main>
-            <form className="controls">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
+                    className="form--input"
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
+                    className="form--input"
                 />
-                <button>Get a new image</button>
-            </form>
+                <button 
+                    className="form--button"
+                    onClick={getNewImage}
+                >
+                    Get a new meme image
+                </button>
+            </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
