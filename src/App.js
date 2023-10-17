bc439a8123b2cbb25d86087e7ebf0965fea77144
jordand2003 <jordandominguez600@gmail.com>
2023-10-17 16:55:23 -0400

    styles for input boxes and image generator created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "../src/components/Header"
+import Body from "../src/components/Body"
 import "./styles.css"
 
 export default function App(){
   return(
     <div>
       <Header />
+      <Body />
     </div>
   )
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -3,9 +3,17 @@ import React from "react"
 export default function Body(){
     return (
         <main>
-            <div>
-                
-            </div>
+            <form className="controls">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                />
+                <button>Get a new image</button>
+            </form>
         </main>
     )
 }
