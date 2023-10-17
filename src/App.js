22d1285a18414d8d86cac4cd4a8959fcbc361465
jordand2003 <jordandominguez600@gmail.com>
2023-10-17 16:16:25 -0400

    Header setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../src/components/Header"
 
 export default function App(){
   return(
     <div>
-      Hello!
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header>
+            <img src="./memegen/images/trollface.png"/>
+            <h2 className="title">Meme Generator</h2>
+            <h3 className="subtitle">Create your own custom memes!</h3>
+        </header>
+    )
+}
