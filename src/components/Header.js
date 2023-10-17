0fcbebd7eb5d88ad19c246d2d036ea28c1eee959
jordand2003 <jordandominguez600@gmail.com>
2023-10-17 16:32:09 -0400

    testing styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header(){
     return(
         <header>
-            <img src="./memegen/images/trollface.png"/>
+            <img src="./memegen/images/trollface.png" alt="img"/>
             <h2 className="title">Meme Generator</h2>
             <h3 className="subtitle">Create your own custom memes!</h3>
         </header>
